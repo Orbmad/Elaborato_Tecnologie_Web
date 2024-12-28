405f5b9481388aa43f831f9883961b21982776aa
Michele Farneti <michele.farneti@studio.unibo.it>
2024-12-28 19:27:41 +0100

    added burger menu icon

--- template/base.php
+++ template/base.php
@@ -12,16 +12,18 @@
         <nav>
             <form action="#" method="GET">
                 <label for="fastsearch">Ricerca rapida</label>
-                    <input type="text" id="fastsearch"/>
-                    <input type="image" src="upload/search-icon.png" alt="Submit">
+                    <input type="text" id="fastsearch" placeholder="Cerca la tua pianta.."/>
                     <input type="image" src="upload/cancel-icon.png" alt="Reset">
+                    <input type="image" src="upload/search-icon.png" alt="Submit">
             </form>
+
+            <img class="menu-icon" src="upload/menu-icon.png" alt="Menu icon">
         </nav>
         <main>
 
         </main>
         <footer>
-
+            <a href="https://www.flaticon.com/free-icons/menu-burger" title="menu-burger icons">Menu-burger icons created by O.moonstd - Flaticon</a>
         </footer>
     </body>
 </html>
