f0f6e9ecfc1d9c0583ef7ff8378ab48437124e68
Manuele D'Ambrosio <manuele.dambrosio@studio.unibo.it>
2024-12-28 23:25:59 +0100

    Changed nav desktop mode

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,9 @@
 session_start();
 require_once("utils/functions.php");
 require_once("database/database.php");
+
+/*Provvisorio per i test*/
+require_once("dummyTemplateParams.php");
+
 $dbh = new DatabaseHelper("localhost", "root", "", "Plantatio", 3306);
 ?>
--- dummyTemplateParams.php
+++ dummyTemplateParams.php
@@ -1,3 +1,5 @@
 <?php
-
+    $templateParams["categorie"]["categoria1"]["nome_categoria"] = "NomeCategoria1";
+    $templateParams["categorie"]["categoria2"]["nome_categoria"] = "NomeCategoria2";
+    $templateParams["categorie"]["categoria3"]["nome_categoria"] = "NomeCategoria3";
 ?>
