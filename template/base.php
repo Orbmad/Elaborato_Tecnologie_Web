f215b0311744ad6f67f7b9c25c58b43930450b91
Manuele D'Ambrosio <manuele.dambrosio@studio.unibo.it>
2024-12-28 12:04:09 +0100

    Deafault

---
+++ template/base.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title><?php echo $templateParams["titolo"]; ?></title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css" />
+    </head>
+    <body>
+        <header>
+            <h1>Plantatio</h1>
+        </header>
+        <nav>
+        
+        </nav>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
