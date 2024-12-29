ce7559f999523bb00b64e6c4abc1d4c767f9db56
Manuele D'Ambrosio <manuele.dambrosio@studio.unibo.it>
2024-12-30 00:25:39 +0100

    Fixed categories in desktop mode

--- dummyTemplateParams.php
+++ dummyTemplateParams.php
@@ -2,4 +2,6 @@
     $templateParams["categorie"]["categoria1"]["nome_categoria"] = "NomeCategoria1";
     $templateParams["categorie"]["categoria2"]["nome_categoria"] = "NomeCategoria2";
     $templateParams["categorie"]["categoria3"]["nome_categoria"] = "NomeCategoria3";
+    $templateParams["categorie"]["categoria4"]["nome_categoria"] = "NomeCategoria4";
+    $templateParams["categorie"]["categoria5"]["nome_categoria"] = "NomeCategoria5";
 ?>
