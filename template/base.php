87c0d96847cf1c56400ab09538faa37b03d4104c
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2024-12-29 15:33:57 +0100

    definition of the header

--- template/base.php
+++ template/base.php
@@ -7,7 +7,20 @@
     </head>
     <body>
         <header>
-            <h1>Plantatio</h1>
+        <h1>Platatio
+            </h1><ul>
+            <!--Creazione di una lista per gestire con css il fatto che la lista si trova a dx della pagina-->
+                <li  id="icon">
+                    <a href="#"><img src=".\img\Icona-Utente.png" alt="user-icon"/></a>
+                    <ul>
+                        <li><a href="#">Esci</a></li>
+                        <li><a href="#">Lista dei desideri</a></li>
+                        <li><a href="#">Notifiche</a></li>
+                    </ul>
+                </li><li>
+                    <a href="#"><img src=".\img\Icona-Carrello.jpg" alt="cart-icon"/></a>
+                </li>
+            </ul>
         </header>
         <nav>
         
