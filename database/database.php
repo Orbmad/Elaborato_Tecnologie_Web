6e5d4ad8a68ed4c5659d6a24f02da5357b1efb4c
Michele Farneti <michele.farneti@studio.unibo.it>
2025-01-02 20:13:30 +0100

    Created function used for getting random groups from the db

--- database/database.php
+++ database/database.php
@@ -25,5 +25,18 @@ class DatabaseHelper{
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
+
+    /**
+     * Function returning n random groups from the groups table, articles for the
+     * main page slideshow will be made out of them.
+     */
+    public function getArticles($n){
+        $stmt = $this->db->prepare("SELECT nomegruppo, descrizionegruppo FROM gruppi ORDER BY RAND() LIMIT ?");
+        $stmt->bind_param('i',$n);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
 ?>
