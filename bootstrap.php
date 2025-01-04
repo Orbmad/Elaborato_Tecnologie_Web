8b23b70331c8d0cdb9c8d18e509eb588e299050b
Manuele D'Ambrosio <manuele.dambrosio@studio.unibo.it>
2025-01-04 17:08:37 +0100

    fixed bootstrap.php script push

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,7 @@ require_once("utils/functions.php");
 require_once("database/database.php");
 
 //JS scripts
+$templateParams["js"] = array();
 array_push($templateParams["js"], "js/nav.js");
 
 /*Provvisorio per i test*/
