1e3c0ef07da412ed755f43b9d751fdaaf00847c9
Michele Farneti <michele.farneti@studio.unibo.it>
2025-01-07 13:00:13 +0100

    Imporvementes to the search bar

--- search.php
+++ search.php
@@ -7,13 +7,13 @@ $templateParams["js"] = array("js/search.js");
 
 $templateParams["asideContent"] = "template/filter-panel.php";
 $templateParams["priceRange"] = $dbh->getProductsPrinceRange();
-$templateParams["family"] = $dbh->getProductsAttributeValues("famiglia");
-$templateParams["genre"] = $dbh->getProductsAttributeValues("genere");
-$templateParams["spiece"] = $dbh->getProductsAttributeValues("specie");
-$templateParams["dimensions"] = $dbh->getProductsAttributeValues("dimensioni");
-$templateParams["perfume"] = $dbh->getProductsAttributeValues("profumo");
-$templateParams["leafType"] = $dbh->getProductsAttributeValues("tipologia_foglia");
-$templateParams["leafColors"] = $dbh->getProductsAttributeValues("colore_foglia");
+$templateParams["famiglia"] = $dbh->getProductsAttributeValues("famiglia");
+$templateParams["genere"] = $dbh->getProductsAttributeValues("genere");
+$templateParams["specie"] = $dbh->getProductsAttributeValues("specie");
+$templateParams["dimensioni"] = $dbh->getProductsAttributeValues("dimensioni");
+$templateParams["profumo"] = $dbh->getProductsAttributeValues("profumo");
+$templateParams["tipo di foglia"] = $dbh->getProductsAttributeValues("tipologia_foglia");
+$templateParams["colore delle foglie"] = $dbh->getProductsAttributeValues("colore_foglia");
 
 require 'template/base.php';
 ?>
--- template/filter-panel.php
+++ template/filter-panel.php
@@ -1,31 +1,41 @@
 <form action="search.php" method="GET">
     <label for="#filterd-search"></label>
-    <input type="button" id="filtered-search" value="Più filtri"/>
+    <input type="button" id="filtered-search" value="Più filtri" />
     <section class="filter-price">
         <h3>Prezzo:</h3>
         <section class="min-price">
             <label for="min-price">Prezzo minimo</label>
-            <input type="range" id="min-price" name="min-price" min="<?php echo floatval($templateParams["priceRange"][0]["min"])?>" max="<?php echo floatval($templateParams["priceRange"][0]["max"])?>" value="<?php echo floatval($templateParams["priceRange"][0]["min"])?>" step="0.1">
+            <input type="range" id="min-price" name="min-price"
+                min="<?php echo floatval($templateParams["priceRange"][0]["min"]) ?>"
+                max="<?php echo floatval($templateParams["priceRange"][0]["max"]) ?>"
+                value="<?php echo floatval($templateParams["priceRange"][0]["min"]) ?>" step="0.1">
             <label for="min-price-selected">Prezzo minimo selezionato</label>
-            <input type="text" id="min-price-selected" value="" readonly/>
+            <input type="text" id="min-price-selected" value="" readonly />
         </section>
         <section class="max-price">
             <label for="max-price">Prezzo massimo</label>
-            <input type="range" id="max-price" name="max-price" min="<?php echo floatval($templateParams["priceRange"][0]["min"])?>" max="<?php echo floatval($templateParams["priceRange"][0]["max"])?>" value="<?php echo floatval($templateParams["priceRange"][0]["max"])?>" step="0.2">
+            <input type="range" id="max-price" name="max-price"
+                min="<?php echo floatval($templateParams["priceRange"][0]["min"]) ?>"
+                max="<?php echo floatval($templateParams["priceRange"][0]["max"]) ?>"
+                value="<?php echo floatval($templateParams["priceRange"][0]["max"]) ?>" step="0.2">
             <label for="max-price-selected">Prezzo massimo selezionato</label>
-            <input type="text" id="max-price-selected" value="" readonly/>
+            <input type="text" id="max-price-selected" value="" readonly />
         </section>
     </section>
 
-    <section class="filter-family">
-        <h3>Famiglia:</>
-    </section>
-    <?php 
-        for($i = 0; $i < count($templateParams["family"]);$i++){
-            echo '<input type="checkbox" id="' . $templateParams['family'][$i]["attributo"] . '" name="' . $templateParams['family'][$i]["attributo"] . '" value="family">';
-            echo '<label for="' . $templateParams['family'][$i]["attributo"] . '">' . $templateParams['family'][$i]["attributo"] . '</label>';
-        }
+    <?php $filterAttributes = array("famiglia", "genere", "specie", "dimensioni","profumo","tipo di foglia","colore delle foglie");
+        foreach ($filterAttributes as $filterAttribute):
     ?>
+    <section class="filter-checkbox filter-<?php echo $filterAttribute ?>">
+        <h3><?php echo $filterAttribute ?></h3>
+        <?php
+        for ($i = 0; $i < count($templateParams[$filterAttribute]); $i++) {
+            echo "<section>";
+            echo '<input type="checkbox" id="' . $templateParams[$filterAttribute][$i]["attributo"] . '" name="' . $templateParams[$filterAttribute][$i]["attributo"] . '" value="'. $filterAttribute .'">';
+            echo '<label for="' . $templateParams[$filterAttribute][$i]["attributo"] . '">' . $templateParams[$filterAttribute][$i]["attributo"] . '</label>';
+            echo "</section>";
+        }
+        ?>
+    </section>
+    <?php endforeach;?>
 </form>
-
-<!-- <?php var_dump($templateParams["family"])?> -->
