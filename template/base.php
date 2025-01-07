6b403e1d2f51f8b59a976b91c7e6b01b318ba39e
Michele Farneti <michele.farneti@studio.unibo.it>
2025-01-08 00:10:14 +0100

    Added basic layout for search results

--- template/base.php
+++ template/base.php
@@ -37,7 +37,7 @@
         <form action="search.php" method="GET">
             <label for="fastsearch">Ricerca rapida</label>
             <section class="search-text">
-                <input type="text" id="fastsearch" name="fastsearch" placeholder="Cerca la tua pianta..." oninput="showSuggestions()" />
+                <input type="text" id="fastSearch" name="fastSearch" placeholder="Cerca la tua pianta..." oninput="showSuggestions()" />
                 <ul class="suggestions not-showing">
                     <li class="product-suggested">
                         <a href="#">
@@ -91,6 +91,11 @@
         </button>
 
     </nav>
+    <?php
+    if (isset($templateParams["asideContent"])) {
+        require($templateParams["asideContent"]);
+    }
+    ?>
 
     <main>
         <?php
@@ -99,17 +104,11 @@
         }
         ?>
         <?php
-        var_dump($templateParams["searchResults"]);
-        if (isset($templateParams["searchResults"])) {
-            require($templateParams["searchResults"]);
+        if (isset($templateParams["mainContent"])) {
+            require($templateParams["mainContent"]);
         }
         ?>
     </main>
-    <?php
-    if (isset($templateParams["asideContent"])) {
-        require($templateParams["asideContent"]);
-    }
-    ?>
 
     <footer>
         <a href="https://www.flaticon.com/free-icons/menu-burger" title="menu-burger icons">Menu-burger icons created by
--- template/search-results.php
+++ template/search-results.php
@@ -1,11 +1,16 @@
-<ul>
+<ul class="search-results-list">
     <?php
-    if (isset($_GET['fastSearch'])) {
-        $result = $dbh->searchProductByName($_GET['fastSearch']);
-        var_dump($result);
-    } else {
-
-        // Alternate code
-    }
+        foreach($templateParams["searchResults"] as $result):
+    ?>
+    <li>
+        <a>
+            <img src="./upload/pianta1.jpg" class="product-image" alt="product image"/>
+            <h2><?php echo $result["nome_prodotto"] ?></h2>
+            <p></p>
+            <p></p>
+        </a>
+    </li>
+    <?php
+        endforeach;
     ?>
 </ul>
