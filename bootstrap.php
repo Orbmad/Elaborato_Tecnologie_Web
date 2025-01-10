55de5fb7bfb915fd52797552b3108f8edcbed2a5
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2025-01-10 08:34:53 +0100

    Added images of stars

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,5 @@
 <?php
-
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
+session_start();
 require_once("utils/functions.php");
 require_once("database/database.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "Plantatio", 3306);
