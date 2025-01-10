fad7ac280f150d458230e2a79dcc33741dcf5760
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2025-01-10 23:21:31 +0100

    added notification page

--- product.php
+++ product.php
@@ -6,19 +6,18 @@ $templateParams["titolo"] = "Plantatio";
 
 //Main content
 $templateParams["mainContent"] = "single-item.php";
-$templateParams["js"] = array("js/product.js", "js/nav.js");
+$templateParams["js"] = array("js/product.js", "js/nav.js", "js/index.js");
 //Home Template
 $idprodotto = -1;
 if(isset($_GET["id"])){
     $idprodotto = $_GET["id"];
 }
 
-//$idprodotto = "Adiantum hispidulum";/*capire perchè se lo passo da URL in nome non va*/
 
 /*per aprire la pagina relativa al singolo prodotto è necessario passare il suo ID per
 poter prelevare dal DB le informazioni relative ad esso*/
 $templateParams["prodotto"] = $dbh->getProductById($idprodotto);
-/*$templateParams["voto"] = 2.5;*/
+
 $templateParams["recensioni_prodotto"] = $dbh->getReviewsOfProduct($idprodotto);
 
 require 'template/base.php';
